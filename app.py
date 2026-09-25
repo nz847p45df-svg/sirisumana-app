5b1cc3d414eeaf9c0b781c016d322c182d317dcd
nz847p45df-svg <nz847p45df@privaterelay.appleid.com>
2026-09-25 20:20:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -297,7 +297,7 @@ with tab1:
                 "වෙනත් කිසිදු අමතර සටහනක් නොලියා pure JSON පමණක් ලබාදෙන්න."
             )
             response = client.models.generate_content(
-                model="gemini-1.5-flash", contents=[img, prompt_text]
+                model="gemini-2.5-flash", contents=[img, prompt_text]
             )
             raw_json = (
                 response.text.strip()
